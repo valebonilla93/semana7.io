ef4a27e82824748aafda1faa7dfd52160a95236c
ValeBo46 <valebonilla1993@gmail.com>
2023-08-07 20:13:21 -0300

    Update script.js

--- Ejercicio7_2_fetch_json/js/script.js
+++ Ejercicio7_2_fetch_json/js/script.js
@@ -19,3 +19,5 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL).then((response) => response.json())
+  .then((data) => showData(data.students))
