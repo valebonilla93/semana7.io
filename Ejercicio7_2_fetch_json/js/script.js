64616aee6b3cda79a9e039970632e73277625be4
perezav <astridvictori10@gmail.com>
2023-08-07 20:14:08 -0300

    Update script.js

--- Ejercicio7_2_fetch_json/js/script.js
+++ Ejercicio7_2_fetch_json/js/script.js
@@ -19,5 +19,7 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
-fetch(DATA_URL).then((response) => response.json())
+fetch(DATA_URL)
+  .then((response) => response.json())
   .then((data) => showData(data.students))
+  
